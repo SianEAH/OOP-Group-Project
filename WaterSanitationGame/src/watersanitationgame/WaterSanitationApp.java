93ad80e22f2062a2d38e1fd5cba6140ec39a4487
NikNci <x24465532@student.ncirl.ie>
2025-11-14 12:04:02 +0000

    test

--- WaterSanitationGame/src/watersanitationgame/WaterSanitationApp.java
+++ WaterSanitationGame/src/watersanitationgame/WaterSanitationApp.java
@@ -1,20 +1,17 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-
 package watersanitationgame;
 
 /**
  * 
- * @author
+ * @authors Nikolas Misins, Jesse Duncan, Sian Hayes
  */
 
 //imports
-
+import watersanitationgame.Ending_Jesse.endingGUI;
 
 public class WaterSanitationApp {
     public static void main(String[] args) {
-        
+        //code below is test for synchronising packages
+        endingGUI g = new endingGUI();
+        g.setVisible(true);
     }
 }
